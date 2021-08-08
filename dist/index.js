73d8a318a68f83a886e2fe77b9e1858b88e929fc
Reto Habluetzel <rethab@protonmail.ch>
2021-08-08 15:44:16 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -6,17 +6,39 @@
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.action = void 0;
 const path_1 = __importDefault(__nccwpck_require__(622));
+const fs = __importStar(__nccwpck_require__(747));
 async function action(core, exec) {
     const exitCode = await exec.exec('mypy', ['test'], { ignoreReturnCode: true });
     const matchersDir = path_1.default.join(__dirname, '../');
+    const matcherPath = path_1.default.join(matchersDir, 'mypy.json');
+    core.info(`Matcher file: ${fs.readFileSync(matcherPath, 'utf-8')}`);
     core.info('Add matcher');
-    core.info(`##[add-matcher]${path_1.default.join(matchersDir, 'mypy.json')}`);
+    core.info(`##[add-matcher]${matcherPath}`);
     core.info('After add matcher');
     if (exitCode !== 0) {
         throw new Error('mypy has returned errors');
--- src/action.ts
+++ src/action.ts
@@ -1,6 +1,7 @@
 import * as Core from '@actions/core'
 import * as Exec from '@actions/exec'
 import path from 'path'
+import * as fs from 'fs'
 
 export async function action(
   core: typeof Core,
@@ -9,8 +10,10 @@ export async function action(
   const exitCode = await exec.exec('mypy', ['test'], {ignoreReturnCode: true})
 
   const matchersDir = path.join(__dirname, '../')
+  const matcherPath = path.join(matchersDir, 'mypy.json')
+  core.info(`Matcher file: ${fs.readFileSync(matcherPath, 'utf-8')}`)
   core.info('Add matcher')
-  core.info(`##[add-matcher]${path.join(matchersDir, 'mypy.json')}`)
+  core.info(`##[add-matcher]${matcherPath}`)
   core.info('After add matcher')
 
   if (exitCode !== 0) {
