60762e02a80c24d3505b8faa6c603276f6968e64
Reto Habluetzel <rethab@protonmail.ch>
2021-08-08 14:46:15 +0200

    make it work

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import {getOctokit} from '@actions/github'
 import {action} from './action'
 
 async function run(): Promise<void> {
   try {
-    await action(core, exec)
+    await action(core, exec, getOctokit)
   } catch (error) {
     core.setFailed(error.message)
   }
